c9f5a9d70992634d3c0418e2f209451e31f7c899
Hary <hratsimba@genplus.fr>
2019-01-18 15:51:34 +0100

    Use Spectre.css as stylesheet

--- src/views/app.vue
+++ src/views/app.vue
@@ -5,7 +5,16 @@
   </div>
 </template>
 
+<style>
+  body {
+    padding: 10px 80px;
+  }
+</style>
+
 <script>
+import 'spectre.css/dist/spectre.min.css'
+import 'spectre.css/dist/spectre-icons.min.css'
+
 import Navigation from './navigation'
 
 export default {
--- src/views/navigation.vue
+++ src/views/navigation.vue
@@ -1,27 +1,34 @@
 <template>
-  <div id="navigation">
-    <ul>
-      <li>
-        <router-link to="/gallery/category-1">
-          Catégorie 1
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/gallery/category-2">
-          Catégorie 2
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/gallery/category-3">
-          Catégorie 3
-        </router-link>
-      </li>
-    </ul>
-  </div>
+  <header class="navbar">
+    <section class="navbar-section">
+      <h1 class="navbar-brand mr-2">
+        Gallery
+      </h1>
+      <router-link
+        to="/gallery/category-1"
+        class="btn btn-link"
+      >
+        Catégorie 1
+      </router-link>
+      <router-link
+        to="/gallery/category-2"
+        class="btn btn-link"
+      >
+        Catégorie 2
+      </router-link>
+      <router-link
+        to="/gallery/category-3"
+        class="btn btn-link"
+      >
+        Catégorie 3
+      </router-link>
+    </section>
+    <section class="navbar-section">
+      <div class="input-group input-inline">
+        <span class="label label-rounded label-error py-2">
+          You are offline <i class="icon icon-stop" />
+        </span>
+      </div>
+    </section>
+  </header>
 </template>
-
-<style scoped>
-#navigation {
-  text-align: center;
-}
-</style>
