13b603f0fdfe012701dcac30965a9af9555c54f3
Hary <>
2019-01-19 13:36:15 +0100

    Add the SW fetch callback to serve cached resources

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,5 @@
 const cacheName = 'gallery-v1'
 
-const { assets } = global.serviceWorkerOption
-
-console.log(assets)
-
 // Cache static resources needed to display the app offline
 self.oninstall = event => {
   // Make sure resources are cached to complete the installation
@@ -16,6 +12,24 @@ self.oninstall = event => {
         './vue.js',
         './assets/imgs/no-image-placehoder.jpg'
       ])
+    })
+  )
+}
+
+self.onfetch = event => {
+  event.respondWith(
+    // Return cache resources, including app files registred in the installation phase
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response
+      } else {
+        // The resource is not cached
+        // Fetch only app caches files and API requests
+        // not images, as it may take some space. The user should have the control over its image storage
+
+        console.log('Need to fetch the API and cache the response, then return the response')
+        // TODO: Check the URL to cache only API requests, not imgs
+      }
     })
   )
 }
